64cdfa395fc872f4da7626850e6d12f12e40cb4e
Gabriel R. <gabrielrengifo211@gmail.com>
2025-01-04 17:46:28 -0500

    initial functional set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { marked } from 'marked';
+import { useState } from 'react';
+
+function MarkdownPreviewer() {
+
+  const [text, setText] = useState('');
+
+  function handleChange(event) {
+    setText(event.target.value);
+    renderText(event.target.value);
+  }
+
+  function renderText(text) {
+    const parsed = marked.parse(text);
+    console.log(parsed)
+    document.getElementById('preview').innerHTML = parsed
+  };
+
+  return (
+    <div>
+      <textarea id="editor" onChange={handleChange} />
+      <div id="preview" />
+    </div>
+  )};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Markdown Previewer</h1>
+      <MarkdownPreviewer />
     </div>
   );
 }
