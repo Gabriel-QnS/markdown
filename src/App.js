5a7af26a5d882521aa6d7d4dcf23d865a3edb850
Gabriel R. <gabrielrengifo211@gmail.com>
2025-01-06 18:08:42 -0500

    added some notes and comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import './App.css';
 import { marked } from 'marked';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function MarkdownPreviewer() {
 
-  const [text, setText] = useState('');
+  const initialText = `  # Header 1
+  ## Header 2
+  ### Header 3
+  #### Header 4
+  ##### Header 5
+  ###### Header 6
+  
+  *Italic* _Italic_
+  **Bold** __Bold__
+  ~~Strikethrough~~
+  
+  - Item 1
+  - Item 2
+    - Subitem 1
+    - Subitem 2
+  
+  1. Item 1
+  2. Item 2
+     1. Subitem 1
+     2. Subitem 2
+  
+  [GitHub](https://github.com)
+  
+  ![Alt text](https://via.placeholder.com/150)
+  
+  > This is a blockquote.`;
+    
+  const [text, setText] = useState("");
 
   function handleChange(event) {
     setText(event.target.value);
-    renderText(event.target.value);
   }
 
-  function renderText(text) {
+  function RenderText({ text }) {
     const parsed = marked.parse(text);
-    return parsed
+    return (
+      <div id="preview" dangerouslySetInnerHTML={useState? {__html: parsed} : marked.parse(initialText)}></div>
+    )
   };
 
   return (
     <div>
       <textarea id="editor" onChange={handleChange} />
-      <div id="preview">{renderText}</div>
+      <RenderText text={text} />
     </div>
   )};
 
